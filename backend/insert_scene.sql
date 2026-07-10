4abff1c5b54d6ad8cc614d028ef59c2001bb1973
beishengyixiao0-rgb <beishengyixiao0@gmail.com>
2026-07-10 14:40:48 +0800

    删除了2个多余的文件

--- backend/insert_scene.sql
+++
@@ -1,2 +0,0 @@
-﻿INSERT INTO detection_scenes (name, display_name, category, class_names, is_active, created_at)
-VALUES ('plant_disease', '植物病害检测', 'agriculture', '["healthy_leaf", "powdery_mildew", "leaf_spot"]', true, NOW());
